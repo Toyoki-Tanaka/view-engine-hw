d66cc104997636c8f766b05d51b4ce3eae6ac88b
SamteaSilver <tanakatoyoki19@gmail.com>
2023-09-05 14:36:57 -0500

    Fixed the 10 routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set('views', './views');
 app.set('view engine', 'fruits');
 
 app.get('/apple', (req, res) => {
-    res.render('apple', {
+    res.render('fruit1', {
         name: 'Fuji Apple',
         description: 'The Fuji apple is an apple cultivar developed by growers at Tohoku Research Station in Fujisaki, Aomori, Japan, in the late 1930s, and brought to market in 1962. It originated as a cross between two American apple varieties—the Red Delicious and old Virginia Ralls Janet apples. ',
         color: 'Red',
@@ -30,7 +30,7 @@ app.get('/apple', (req, res) => {
 });
 
 app.get('/banana', (req, res) => {
-    res.render('banana', {
+    res.render('fruit2', {
         name: 'Cavendish',
         description: 'Cavendish bananas are the fruits of one of a number of banana cultivars belonging to the Cavendish subgroup of the AAA banana cultivar group. The same term is also used to describe the plants on which the bananas grow. They include commercially important cultivars like Dwarf Cavendish and Grand Nain',
         color: 'Yellow',
@@ -38,7 +38,7 @@ app.get('/banana', (req, res) => {
 });
 
 app.get('/cherry', (req, res) => {
-    res.render('cherry', {
+    res.render('fruit1', {
         name: 'Bing Cherry',
         description: 'Bing is a cultivar of the wild or sweet cherry that originated in the Pacific Northwest, in Milwaukie, Oregon, United States. The Bing remains a major cultivar in Oregon, Washington, California, Wisconsin and British Columbia. It is the most produced variety of sweet cherry in the United States. ',
         color: 'Dark red',
@@ -46,7 +46,7 @@ app.get('/cherry', (req, res) => {
 });
 
 app.get('/grape', (req, res) => {
-    res.render('grape', {
+    res.render('fruit2', {
         name: 'Concord',
         description: 'The Concord grape is a cultivar derived from the grape species Vitis labrusca that are used as table grapes, wine grapes and juice grapes. They are often used to make grape jelly, grape juice, grape pies, grape-flavored soft drinks, and candy.',
         color: 'Purple',
@@ -54,7 +54,7 @@ app.get('/grape', (req, res) => {
 });
 
 app.get('/kiwi', (req, res) => {
-    res.render('kiwi', {
+    res.render('fruit1', {
         name: 'Actinidia deliciosa',
         description: 'Actinidia deliciosa, the fuzzy kiwifruit, is a fruiting vine native to Southern China. Other species of Actinidia are also found in China and range east to Japan and north into southern areas of Russian Far East. This species grows naturally at altitudes between 600 and 2,000 m.',
         color: 'Brown/Green',
@@ -62,7 +62,7 @@ app.get('/kiwi', (req, res) => {
 });
 
 app.get('/lychee', (req, res) => {
-    res.render('lychee', {
+    res.render('fruit2', {
         name: 'Litchi chenensis',
         description: 'Litchi chinensis subsp. chinensis is the only commercialized lychee. It grows wild in southern China, northern Vietnam, and Cambodia. It has thin twigs, flowers typically have six stamens, fruit are smooth or with protuberances up to 2 mm (0.079 in).',
         color: 'White/Pink',
@@ -70,7 +70,7 @@ app.get('/lychee', (req, res) => {
 });
 
 app.get('/mango', (req, res) => {
-    res.render('mango', {
+    res.render('fruit1', {
         name: 'Ataulfo',
         description: 'The Ataúlfo mango is a mango cultivar from Mexico. Ataulfo mangos are golden yellow and generally weigh between 6 and 10 ounces, with a somewhat sigmoid shape and a gold-yellow skin. The flesh is not fibrous, and the pit is thin. They were named for grower Ataulfo Morales Gordillo.',
         color: 'Yellow/Orange',
@@ -78,7 +78,7 @@ app.get('/mango', (req, res) => {
 });
 
 app.get('/orange', (req, res) => {
-    res.render('orange', {
+    res.render('fruit2', {
         name: 'Jaffa',
         description: 'The Jaffa orange, also known as Shamouti orange, is an orange variety with few seeds and a tough skin that makes it particularly suitable for export. Developed by Arab farmers in the mid-19th century, the variety takes its name from the city of Jaffa where it was first produced for export.',
         color: 'Orange',
@@ -86,7 +86,7 @@ app.get('/orange', (req, res) => {
 });
 
 app.get('/strawberry', (req, res) => {
-    res.render('mango', {
+    res.render('fruit1', {
         name: 'Fragaria moschata',
         description: 'The musk strawberry or hautbois strawberry, is a species of strawberry native to Europe. Its French name hautbois strawberry may be anglicised as hautboy strawberry. The plants are hardy and can survive in many weather conditions. They are cultivated commercially on a small scale, particularly in Italy.',
         color: 'Red',
@@ -94,7 +94,7 @@ app.get('/strawberry', (req, res) => {
 });
 
 app.get('/tamarind', (req, res) => {
-    res.render('tamarind', {
+    res.render('fruit2', {
         name: 'Pithecellobium dulce',
         description: 'Pithecellobium dulce, commonly known as Manila tamarind, Madras thorn, monkeypod tree or camachile, is a species of flowering plant in the pea family, Fabaceae, that is native to the Pacific Coast and adjacent highlands of Mexico, Central America, and northern South America',
         color: 'Brown',
